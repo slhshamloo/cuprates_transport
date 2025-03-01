fixed omega sign problem

cuprates_transport/conductivity.py
@@ -275,17 +275,18 @@ class Conductivity:
                 vj = np.sum(vft[:, :, :] * exp(-t_o_tau) * self.dtime, axis=2) # (3, len_kf)
             else:
                 vj = np.sum(vft[:, :, :] * exp(-t_o_tau) * self.dtime
-                            * exp(-1j * self.omega * self.time_array), axis=2) # (3, len_kf)
+                            * exp(1j * self.omega * self.time_array), axis=2) # (3, len_kf)
             self.v_product = np.einsum('ji,ki->jki',vi,vj) # (3, 3, len_kf)
                                         # with 3, 3 the indices for i, j in sigma
         else:
-            if self.omega != 0:
-                vj = np.sum(vft[:, :, :] * exp((-1j * self.omega - t_o_tau[:, None])
-                    * self.time_array) * self.dtime, axis=2) # (3, len_kf)
-                self.v_product = np.einsum('ji,ki->jki', vft[:, :, 0], vj)
-            else:
+            if self.omega == 0:
                 self.v_product = np.einsum(
                     'ji,ki->jki', vft[:, :, 0], vft[:, :, 0] * (1 / t_o_tau))
+            else:
+                vj = np.sum(vft[:, :, :] * exp((1j * self.omega - t_o_tau[:, None])
+                    * self.time_array) * self.dtime, axis=2) # (3, len_kf)
+                self.v_product = np.einsum('ji,ki->jki', vft[:, :, 0], vj)
+            
         return self.v_product
 
     def sigma_epsilon(self, dos_k, dkf, kft, vft, t_o_tau):

data/free_electron/sigmaxx__w_0_150__B_0_3000.py
@@ -89,8 +89,7 @@ for B in fieldValues:
   w =  omegaValues * 1e12 # rad.s-1
   Ga = params["gamma_0"] * 1e12 # rad.s-1
   wc_sq = e**2*B**2/m**2 # (rad.s-1)2
-  # Conjugate because of time-derivative conventions (Legros et al. vs cuprates_transport)
-  results[:,2] = np.conjugate(1j*n*e**2/m * (w + 1j*Ga) / ((w + 1j*Ga)**2 - wc_sq))
+  results[:,2] = 1j*n*e**2/m * (w + 1j*Ga) / ((w + 1j*Ga)**2 - wc_sq)
 
   path = os.path.relpath(__file__)
   dirname, fname = os.path.split(path)

plots/chambers_fits/chambers_fit_omega_parallel.py
@@ -33,7 +33,7 @@ def chambers_residual(param_values, param_keys, omegas, sigmas,
         setattr(cond_obj, "omega", omega)
         cond_obj.chambers_func()
         sigma_fit[i] = (cond_obj.sigma[0, 0] + 1j*cond_obj.sigma[0, 1]
-                        ).conjugate() * 1e-5
+                        ) * 1e-5
     if reduce_error:
         return np.sum(np.abs(sigmas-sigma_fit)**2) / (
             len(omegas)-len(param_keys))
@@ -70,7 +70,7 @@ def chambers_residual_multi_field(
             setattr(cond_obj, "omega", omega)
             cond_obj.chambers_func()
             sigma_fit[i, j] = (cond_obj.sigma[0, 0] + 1j*cond_obj.sigma[0, 1]
-                               ).conjugate() * 1e-5
+                               ) * 1e-5
 
     if reduce_error:
         return np.sum(np.abs(sigmas-sigma_fit)**2) / (

plots/chambers_fits/explore.py
@@ -18,7 +18,7 @@ def chambers_residual(omegas, sigmas, band_obj, params):
         setattr(cond_obj, "omega", omega)
         cond_obj.chambers_func()
         sigma_fit[i] = (cond_obj.sigma[0, 0] + 1j*cond_obj.sigma[0, 1]
-                        ).conjugate() * 1e-5
+                        ) * 1e-5
 
     return np.mean(np.abs(sigmas-sigma_fit) ** 2)
 
@@ -47,16 +47,16 @@ def get_init_params():
         "a": 3.75,
         "b": 3.75,
         "c": 13.2,
-        "energy_scale": 160,
+        "energy_scale": 70,
         "band_params":{"mu":-0.758, "t": 1, "tp":-0.12,
                        "tpp":0.06, "tz": 0.07},
         "res_xy": 20,
         "res_z": 5,
         "N_time": 1000,
         "Bamp": 9,
-        "gamma_0": 8.0,
-        "gamma_k": 1e4,
-        "power": 5.59,
+        "gamma_0": 10.0,
+        "gamma_k": 5,
+        "power": 6,
         # "gamma_dos_max": 50,
         "march_square": True,
         "parallel": False
@@ -83,7 +83,7 @@ def calculate_residual_plane(
     band_obj = BandStructure(**get_init_params())
     band_obj.runBandStructure()
     residuals = np.empty_like(x_vals)
-    with ProcessPoolExecutor(max_workers=12) as executor:
+    with ProcessPoolExecutor(max_workers=8) as executor:
         for (i, res) in enumerate(tqdm(executor.map(
                 calculate_residual, [x_label]*x_vals.size,
                 [y_label]*y_vals.size, x_vals, y_vals,
@@ -117,20 +117,17 @@ def plot_residual_plane(xy, residuals, vary_x_label, vary_y_label,
 
 
 def main():
+    extra_label = "_gamma_0_10"
     # xy, residuals = calculate_residual_plane(
-    #     "power", "gamma_k", [1, 15], [1.0, 1e4], 10, 10,
-    #     "legros", "OD17K", 9, extra_label="_gamma_0_8")
-    extra_label = "_power_8"
+    #     "gamma_0", "gamma_k", [1, 15], [5e2, 5e3], 10, 10,
+    #     "legros", "OD17K", 31, extra_label=extra_label)
     xy = np.load(os.path.dirname(os.path.relpath(__file__))
                  + f"/explore/xy_OD17K_9T{extra_label}.npy")
     residuals = np.load(os.path.dirname(os.path.relpath(__file__))
                         + f"/explore/residuals_OD17K_9T{extra_label}.npy")
-    # plot_residual_plane(xy, residuals, r"$\nu$", r"$\Gamma_k$",
-    #     r"Residuals for OD17K at 9T, $\Gamma_0=8$",
-    #     filename=f"residuals_OD17K_9T{extra_label}_power_gamma_k.pdf")
-    plot_residual_plane(xy, residuals, r"$\Gamma_0$", r"$\Gamma_k$",
-        r"Residuals for OD17K at 9T, $\nu_0=7.8$",
-        filename=f"residuals_OD17K_9T{extra_label}_gamma_0_gamma_k.pdf")
+    plot_residual_plane(xy, residuals, r"$\nu_0$", r"$\Gamma_k$",
+        r"Residuals for OD17K at 9T, $\Gamma_0=10$",
+        filename=f"residuals_OD17K_9T{extra_label}_power_gamma_k.pdf")
 
 
 if __name__ == "__main__":