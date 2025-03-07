f81ffea52184ee0408200fab7a9fa960a1d99472
Matéo RIVERA <27432367+CeType@users.noreply.github.com>
2025-03-07 08:16:20 +0100

    Nonbreaking extract

--- plots/chambers_fits/plot_chambers_fits.py
+++ plots/chambers_fits/plot_chambers_fits.py
@@ -6,6 +6,9 @@ from matplotlib import pyplot as plt
 from cuprates_transport.bandstructure import BandStructure
 from cuprates_transport.conductivity import Conductivity
 
+import file_operations
+import defaults
+
 
 mpl.rcdefaults()
 # mpl.rcParams['text.usetex'] = True
@@ -26,105 +29,13 @@ mpl.rcParams['axes.linewidth'] = 1.0
 # TrueType allows editing the text in illustrator
 mpl.rcParams['pdf.fonttype'] = 3
 
+load_data = file_operations.load_data
+load_fit = file_operations.load_fit
+generate_chambers_fit = file_operations.generate_chambers_fit
+load_fit_output_data = file_operations.load_fit_output_data
+save_fit_output_data = file_operations.save_fit_output_data
+get_init_params = defaults.get_init_params
 
-def load_data(paper, sample, field):
-    omega_dict = {}
-    sigma_dict = {}
-    for real_imag in ('1', '2'):
-        for side in ('l', 'r'):
-            omega_dict[real_imag+side], sigma_dict[real_imag+side] = (
-                np.loadtxt(f"data/{paper}/{sample}_sigma"
-                           f"{real_imag}{side}_{field}T.csv",
-                           skiprows=1, delimiter=',', unpack=True))
-    omega = np.concatenate((omega_dict['1l'], omega_dict['1r']))
-    sigma1 = np.concatenate((sigma_dict['1l'], sigma_dict['1r']))
-    sigma2 = np.concatenate((
-        np.interp(omega_dict['1l'], omega_dict['2l'], sigma_dict['2l']),
-        np.interp(omega_dict['1r'], omega_dict['2r'], sigma_dict['2r'])))
-    sigma = sigma1 + 1j * sigma2
-    return omega, sigma
-
-
-def get_init_params():
-    return {
-        "band_name": "Nd-LSCO",
-        "a": 3.75,
-        "b": 3.75,
-        "c": 13.2,
-        "energy_scale": 160,
-        "band_params":{"mu":-0.758, "t": 1, "tp":-0.12,
-                       "tpp":0.06, "tz": 0.07},
-        "res_xy": 20,
-        "res_z": 5,
-        "N_time": 1000,
-        "Bamp": 45,
-        "gamma_0": 8.0, # 12.595,
-        "gamma_k": 3e3, # 63.823,
-        "power": 6.2,
-        # "gamma_dos_max": 50,
-        "march_square": True
-    }
-
-
-def load_fit(sample, field):
-    parameter_values = {}
-    parameter_errors = {}
-    with open(os.path.dirname(os.path.relpath(__file__))
-              + f"/params/{sample}_{field}T.dat", 'r') as f:
-        while True:
-            line = f.readline()
-            if not line:
-                return parameter_values, parameter_errors
-            if "[[Variables]]" in line:
-                break
-        line = f.readline()
-        while line and not re.match(r"\[\[[\w\s]*\]\]", line):
-            if line:
-                value_name = line.strip().split()[0][:-1]
-                parameter_values[value_name] = float(line.split()[1])
-                parameter_errors[value_name] = line.split()[3]
-            line = f.readline()
-    return parameter_values, parameter_errors
-
-
-def generate_chambers_fit(sample, field, omegas, bypass_fit=False,
-                          init_params=get_init_params()):
-    parameter_values, _ = load_fit(sample, field)
-    params = deepcopy(init_params)
-    params['Bamp'] = field
-    if not bypass_fit:
-        for parmeter_key, parameter_value in parameter_values.items():
-            if parmeter_key in params:
-                params[parmeter_key] = parameter_value
-            else:
-                params['band_params'][parmeter_key] = parameter_value
-    band_obj = BandStructure(**params)
-    band_obj.runBandStructure()
-    cond_obj = Conductivity(band_obj, **params)
-    cond_obj.runTransport()
-    sigma = np.empty_like(omegas, dtype=complex)
-    for i, omega in enumerate(omegas):
-        setattr(cond_obj, "omega", omega)
-        cond_obj.chambers_func()
-        sigma[i] = (cond_obj.sigma[0, 0] + 1j*cond_obj.sigma[0, 1]
-                    ).conjugate() * 1e-5
-    return sigma
-
-
-def save_fit_output_data(sample, field, omegas):
-    sigma = generate_chambers_fit(sample, field, 2*np.pi*omegas)
-    np.savetxt(os.path.dirname(os.path.relpath(__file__))
-               + f"/outputs/{sample}_{field}T.csv",
-               np.column_stack((omegas, sigma.real, sigma.imag)),
-               delimiter=",", header="omega,sigma_real,sigma_imag")
-
-
-def load_fit_output_data(sample, field):
-    omega, sigma_real, sigma_imag = np.loadtxt(
-        os.path.dirname(os.path.relpath(__file__))
-        + f"/outputs/{sample}_{field}T.csv",
-        delimiter=",", skiprows=1, unpack=True)
-    return omega, sigma_real + 1j * sigma_imag
 
 
 def generate_figure(figsize=(9.2, 5.6)):
